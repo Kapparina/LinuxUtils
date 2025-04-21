b0e80603877a9b87e5094afe444c0e3f7797cf29
Kapparina <116474667+Kapparina@users.noreply.github.com>
2025-04-22 01:06:50 +1000

    feat(watch): enhance directory watch handling
    - Add recursive directory watch support with `walkFunc`.
    - Introduce `eventOccurrence` for managing fsnotify events.
    - Update logging and error handling for watcher operations.
    - Remove unused `addToWatchList` function.
    
    Signed-off-by: Kapparina <116474667+Kapparina@users.noreply.github.com>

--- cmd/watch/main.go
+++ cmd/watch/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"io/fs"
+	"math"
 	"os"
 	"os/signal"
+	"path/filepath"
 	"slices"
 	"strings"
 	"sync"
@@ -12,11 +14,25 @@ import (
 
 	"github.com/charmbracelet/log"
 	"github.com/fsnotify/fsnotify"
+	"github.com/pkg/errors"
 
 	"LinuxUtils/cmd/input"
 	"LinuxUtils/cmd/watch/logging"
 	"LinuxUtils/pkg/io"
 )
+
+type eventOccurrence struct {
+	event fsnotify.Event
+	timer *time.Timer
+	op    fsnotify.Op
+}
+
+func (e eventOccurrence) Matches(v eventOccurrence) bool {
+	if e.event.Name != v.event.Name || e.op&v.op != 0 {
+		return false
+	}
+	return true
+}
 
 var (
 	targetDir string
@@ -76,14 +92,49 @@ func watchLoop(watcher *fsnotify.Watcher, quit chan struct{}) {
 	var (
 		waitDuration = 100 * time.Millisecond
 		mu           sync.Mutex
-		timers       = make(map[string]*time.Timer)
+		occurrences  = make(map[string]eventOccurrence)
 		logCallback  = func(e fsnotify.Event) {
 			logging.LogEvent(targetDir, e)
 			mu.Lock()
-			delete(timers, e.Name)
+			delete(occurrences, e.Name)
 			mu.Unlock()
 		}
 	)
+	walkFunc := func(dir string) {
+		recChan := make(chan string, 100)
+
+		var wg sync.WaitGroup
+		wg.Add(1)
+
+		var err error
+		go func() {
+			defer wg.Done()
+			err = walkRecursively(dir, recChan)
+		}()
+		go func() {
+			wg.Wait()
+			close(recChan)
+		}()
+		for p := range recChan {
+			if itemInWatchList(p, watcher) {
+				continue
+			}
+			if err = watcher.Add(p); err != nil {
+				if errors.Is(err, os.ErrNotExist) {
+					log.Debug("Path does not exist", "path", p)
+					continue
+				}
+				if errors.Is(err, os.ErrPermission) {
+					log.Debug("Path is not readable", "path", p)
+					continue
+				}
+				log.Warn("Failed to add to watcher", "error", err, "path", p)
+			} else {
+				log.Debug("Added to watcher", "path", p)
+			}
+		}
+	}
+	go walkFunc(targetDir)
 	for {
 		select {
 		case err, ok := <-watcher.Errors:
@@ -97,57 +148,69 @@ func watchLoop(watcher *fsnotify.Watcher, quit chan struct{}) {
 			if !ok {
 				return
 			}
-			isDir, _ := io.ValidatePath(event.Name)
-			if event.Name != targetDir && isDir {
-				if added, addErr := addToWatchList(event.Name, watcher); !added && addErr != nil {
-					watcher.Errors <- addErr
+			isDir, err := io.ValidatePath(event.Name)
+			if event.Name != targetDir && err == nil && isDir {
+				if event.Has(fsnotify.Create) || event.Has(fsnotify.Write) {
+					log.Debug("New directory detected", "path", event.Name)
+					go walkFunc(event.Name)
 				}
 			}
 			log.Debug("Event", "event", event)
-			logging.LogEvent(targetDir, event)
+			switch {
+			}
 			if event.Has(fsnotify.Remove) || event.Has(fsnotify.Rename) {
 				if exists := itemInWatchList(event.Name, watcher); exists {
-					// if err = watcher.Remove(event.Name); err != nil && !errors.Is(err, fsnotify.ErrNonExistentWatch) {
-					// 	watcher.Errors <- err
-					// }
+					if err := watcher.Remove(event.Name); err != nil && !errors.Is(err, fsnotify.ErrNonExistentWatch) {
+						watcher.Errors <- err
+					}
 				}
 			}
+			mu.Lock()
+			o, ok := occurrences[event.Name]
+			if !ok {
+				o.timer = time.AfterFunc(math.MaxInt64, func() { logCallback(event) })
+				occurrences[event.Name] = o
+			}
+			mu.Unlock()
+			o.timer.Reset(waitDuration)
 		}
 	}
 }
 
-func walkRecursively(path string, returnChan chan<- string) error {
-	// files, err := os.ReadDir(path)
-	// if err != nil {
-	// 	return
-	// }
-	return fs.WalkDir(os.DirFS(path), ".", func(path string, d fs.DirEntry, err error) error {
+func walkRecursively(rootPath string, returnChan chan<- string) error {
+	fileSystem := os.DirFS(rootPath)
+	return fs.WalkDir(fileSystem, ".", func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return fs.SkipDir
+		}
+		if path == "." {
+			return nil
+		}
 		if d.IsDir() {
-			returnChan <- path
+			fullPath := filepath.Join(rootPath, path)
+			if err != nil {
+				return fs.SkipDir
+			}
+			returnChan <- fullPath
+			log.Debug("Adding to watch list", "path", fullPath)
 			return nil
 		}
 		return nil
 	})
-	// for _, file := range files {
-	// 	if file.IsDir() {
-	// 		returnChan <- file.Name()
-	// 		go walkRecursively(file.Name(), returnChan)
-	// 	}
-	// }
 }
 
 // addToWatchList attempts to add an item to the specified fsnotify.Watcher.
 // Returns true if successful, false if the item is already in the watch list.
 // Returns an error if the addition to the watcher fails.
-func addToWatchList(item string, watcher *fsnotify.Watcher) (bool, error) {
-	if itemInWatchList(item, watcher) {
-		return false, nil
-	}
-	if err := watcher.Add(item); err != nil {
-		return false, err
-	}
-	return true, nil
-}
+// func addToWatchList(item string, watcher *fsnotify.Watcher) (bool, error) {
+// 	if itemInWatchList(item, watcher) {
+// 		return false, nil
+// 	}
+// 	if err := watcher.Add(item); err != nil {
+// 		return false, err
+// 	}
+// 	return true, nil
+// }
 
 func itemInWatchList(item string, watcher *fsnotify.Watcher) bool {
 	watchList := watcher.WatchList()
